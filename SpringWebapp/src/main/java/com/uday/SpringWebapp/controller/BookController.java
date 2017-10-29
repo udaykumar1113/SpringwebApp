8fba724befffd3ae2e61eea7e14c1d946d235fbe
uday <udaykumar1113@gmail.com>
2017-10-29 18:27:14 -0400

    Circular view resolver issue

---
+++ SpringWebapp/src/main/java/com/uday/SpringWebapp/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.uday.SpringWebapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.uday.SpringWebapp.Repositories.BookRepository;
+
+@Controller
+public class BookController {	
+	private BookRepository bookRepository;	
+	public BookController(BookRepository bookRepository) {
+		super();
+		this.bookRepository = bookRepository;
+	}
+	@RequestMapping("/books")
+	public String getBooks(Model model){		
+		model.addAttribute("books",bookRepository.findAll());
+		return "books";
+	}
+
+}
